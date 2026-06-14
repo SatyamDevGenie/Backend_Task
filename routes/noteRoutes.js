b04861dc0a17d08cfb03e74a1f2433013db67bc4
SatyamDevGenie <satyamsawant54@gmail.com>
2026-06-14 23:11:34 +0530

    Created routes for Notes Features

---
+++ routes/noteRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const {
+    createNote,
+    getNotes,
+    getNoteById,
+    updateNote,
+    deleteNote,
+    searchNotes
+} = require("../controllers/noteController");
+
+const router = express.Router();
+
+router.post("/", createNote);
+
+router.get("/", getNotes);
+
+router.get("/search", searchNotes);
+
+router.get("/:id", getNoteById);
+
+router.put("/:id", updateNote);
+
+router.delete("/:id", deleteNote);
+
+module.exports = router;
